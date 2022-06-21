3a121cec2c9f8d72968885fc7be2e120ea0f09d2
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-21 17:41:59 -0400

    set up path, PORT and app variables; added GET request to redirect user to index.html from base path; added listen method to run server on localhost:PORT

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+// When the path is the base path,
+app.get('/', (req, res) =>
+    // the user is redirected to the index.html page
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
