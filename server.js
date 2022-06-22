e304ab057504c084358e2f151642e3353b3ae787
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-21 23:39:02 -0400

    formatted server.js; changed path route in deleteNote() of index.js

--- server.js
+++ server.js
@@ -42,21 +42,21 @@ app.post('/notes/api', (req, res) => {
         };
         const dbData = fs.readFileSync('./db/db.json', 'utf8');
         const notes = dbData.length ? JSON.parse(dbData) : [];
-    
+
         notes.push(newNote);
-    
+
         const noteString = JSON.stringify(notes, null, 2);
-    
-        fs.writeFile(`./db/db.json`, noteString, (err) =>
-        err
-          ? console.error(err)
-          : console.log(`Note has been written to JSON file`)
+
+        fs.writeFile('./db/db.json', noteString, (err) =>
+            err
+                ? console.error(err)
+                : console.log(`Note has been written to JSON file`)
         );
 
         const response = {
             status: 'success',
             body: newNote,
-          };
+        };
 
         res.status(201).json(response);
     } else {
