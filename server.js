d5606873bdb9208ff811a4eb29b8b6e8c97d93de
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-22 11:17:29 -0400

    changed file name utils.js to index.js; adjusted import path in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const { uuid } = require('./utils/util.js');
+const { uuid } = require('./utils');
 
 const PORT = 3001;
 
