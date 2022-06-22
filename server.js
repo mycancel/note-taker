553d36f35818eb4e06f66d82f2b0df7d11464a0d
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-22 12:23:08 -0400

    changed port; staged for heroku push

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const baseRoute = require('./routes');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
